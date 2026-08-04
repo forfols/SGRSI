9384d8451fc88a4e58eea6409ea3d7dae60e0d87
Salvador Medina <salvamed@proton.me>
2026-08-04 09:59:50 -0300

    fix: actualización de procesarInicioSesion.php para ser compatible con PDO

--- proyecto/app/controlador/procesarInicioSesion.php
+++ proyecto/app/controlador/procesarInicioSesion.php
@@ -1,12 +1,17 @@
 <?php
 
-require_once __DIR__ . "/../modelo/Usuario.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once __DIR__ . "/../modelo/AccesoDatosUsuario.php";
+require_once __DIR__ . "/../modelo/ConectorPDO.php";
 require_once __DIR__ . "/../modelo/InicioSesion.php";
+require_once __DIR__ . "/../modelo/Usuario.php";
 
 //Comprueba que el formulario haya sido enviado mediante POST
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
-    header("Location: inicioSesion.php");
+    $mensaje = "Acceso Denegado: Petición incorrecta";
+    header("Location: /proyecto/public/inicioSesion.php?error=" . urlencode($mensaje));
     exit;
 }
 
@@ -14,14 +19,21 @@ if ($_SERVER["REQUEST_METHOD"] !== "POST") {
 $ci = trim($_POST["ci"] ?? "");
 $contra = $_POST["contra"] ?? "";
 
-$accesoDatosUsuario = new AccesoDatosUsuario();
-$inicioSesion = new InicioSesion($accesoDatosUsuario);
+//Credenciales hardcodeadas, en un futuro van a colocarse en archivos aislados o variables de entorno
+$conectorPDO = new ConectorPDO ("localhost:3306", "salva", "", "SGRSI");
+$conexion = $conectorPDO->establecerConexion();
 
-$usuario = $inicioSesion->autenticar($ci, $contra);
+    $accesoDatosUsuario = new AccesoDatosUsuario($conexion);
+    $inicioSesion = new InicioSesion($accesoDatosUsuario);
+    $usuario = $inicioSesion->autenticar($ci, $contra);
+
+$conectorPDO->desconectar();
 
 //Si las credenciales no coinciden, muestra el error y detiene el proceso
 if ($usuario === null) {
-    exit("La cédula o la contraseña son incorrectas.");
+    $mensaje = "Acceso Denegado: La cédula o la contraseña son incorrectas.";
+    header("Location: /proyecto/public/inicioSesion.php?error=" . urlencode($mensaje));
+    exit;
 }
 
 session_start();
@@ -32,12 +44,12 @@ $_SESSION["rol"] = $usuario->getRol();
 
 $rol= $usuario->getRol();
 
-if($rol== "Solicitante") {
-    header("Location: indexSolicitante.php");
-}else if($rol== "Tecnico") {
-    header("Location: tecnico.php");
-}else if ($rol== "Administrador"){
+if($rol== "Administrador") {
     header("Location: indexAdministrador.php");
+}else if($rol== "Solicitante") {
+    header("Location: indexSolicitante.php");
+}else if ($rol== "Tecnico"){
+    header("Location: tecnico.php");
 }
 
 
